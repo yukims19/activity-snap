0d9b8d231ebba98655c390389056fe9b6a6bff39
yukims19 <yukims19@gmail.com>
2023-07-30 16:45:24 -0700

    Initial commit with React + Flask

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  async function getData() {
+    const res = await fetch("http://localhost:7400/flask/hello");
+    const data = await res.json();
+    setMessage(data.message);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>To get message: </p>
+        <button onClick={getData}>Click me</button>
+        <div>Message: {message}</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
