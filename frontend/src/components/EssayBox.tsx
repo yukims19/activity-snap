d6b3d03d25458905e1f7d978d5f69175fc285658
yukims19 <yukims19@gmail.com>
2023-08-09 20:24:18 -0700

    Add essay box UI

--- frontend/src/components/EssayBox.tsx
+++ frontend/src/components/EssayBox.tsx
@@ -1,6 +1,21 @@
-import { Paper } from "@mui/material";
+import styled from "@emotion/styled";
+import { Button, Paper } from "@mui/material";
+import { useState } from "react";
+
+const EssayWrapper = styled.div`
+  border: 1px solid #ba68c880;
+  border-radius: 8px;
+  padding: 24px;
+  margin-top: 24px;
+  min-height: 200px;
+`;
 
 export function EssayBox() {
+  const [essay, setEssay] = useState("");
+
+  function handleSubmission() {
+    setEssay("summary of the day");
+  }
   return (
     <Paper
       elevation={2}
@@ -8,7 +23,10 @@ export function EssayBox() {
         padding: "24px",
       }}
     >
-      <div> hi</div>
+      <Button variant="contained" onClick={handleSubmission} color="secondary">
+        Generate Summary
+      </Button>
+      <EssayWrapper>{essay}</EssayWrapper>
     </Paper>
   );
 }
